2ee19ed9e79a02c74a29227da35fc16a2b0e5cc8
JunYong <jypark3@yookgak.com>
2022-11-30 17:14:11 +0900

    feat: border 제거- 2

--- src/components/Box/Box_1.tsx
+++ src/components/Box/Box_1.tsx
@@ -2,7 +2,17 @@ import { Box } from '@mui/material';
 import { PropsWithChildren } from 'react';
 
 const Box_1 = (props: PropsWithChildren) => {
-  return <Box p={2}> {props.children}</Box>;
+  return (
+    <Box
+      p={2}
+      sx={{
+        border: '1px solid black',
+      }}
+    >
+      {' '}
+      {props.children}
+    </Box>
+  );
 };
 
 export default Box_1;
--- src/components/Box/Box_2.tsx
+++ src/components/Box/Box_2.tsx
@@ -2,17 +2,7 @@ import { Box } from '@mui/material';
 import { PropsWithChildren } from 'react';
 
 const Box_2 = (props: PropsWithChildren) => {
-  return (
-    <Box
-      p={1}
-      sx={{
-        border: '1px solid blue',
-        borderRadius: '4px',
-      }}
-    >
-      {props.children}
-    </Box>
-  );
+  return <Box p={1}>{props.children}</Box>;
 };
 
 export default Box_2;
